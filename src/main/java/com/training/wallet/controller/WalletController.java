a973f31e32de08d0be0c9d091dd0b9e1888958ca
mrtzmhmmd <mrtz.mhmmd@yahoo.com>
2024-02-29 23:37:31 +0330

    Refactor code

--- src/main/java/com/training/wallet/controller/WalletController.java
+++ src/main/java/com/training/wallet/controller/WalletController.java
@@ -4,7 +4,6 @@ import com.training.wallet.dto.request.RequestWalletDto;
 import com.training.wallet.dto.response.BalanceDto;
 import com.training.wallet.dto.response.WalletCreateDto;
 import com.training.wallet.service.WalletService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
